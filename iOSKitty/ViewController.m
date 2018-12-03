43b4b0c87bd7afd710c4d8a5d600d924dd0ca855
Kasia McLellan <kmclella@villanova.edu>
2018-12-02 20:21:25 -0500

    Add hello cat label

--- iOSKitty/ViewController.m
+++ iOSKitty/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *testLabel;
 
 @end
 
@@ -16,6 +17,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //Really Old Way
+    [self.testLabel setText:[NSString stringWithFormat:@"Hello Grandpa Cat"]];
+    //Old Way
+    [self.testLabel setText:@"Hello Kitty"];
+    //New Way-- Use this way!
+    self.testLabel.text = @"hello cat";
+    self.testLabel.textAlignment = NSTextAlignmentCenter;
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
